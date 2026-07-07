e1d10bd7df88f9019aff71e472b1ec702a96b5e4
Iyan S. yue <shiyanpro@gmail.com>
2026-07-07 19:03:16 +0700

    Create config.js with application settings
    Added configuration settings for the Movie Free application.

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// Movie Free by R - Configuration
+const CONFIG = {
+    API_BASE: 'https://api.sonzaix.indevs.in',
+    SITE_NAME: 'Movie Free by R',
+    SITE_TAGLINE: 'Nonton Movie & Series Gratis',
+    PER_PAGE: 20,
+    HERO_BANNER_COUNT: 6,
+};
