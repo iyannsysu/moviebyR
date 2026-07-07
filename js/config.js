a0d0c3338d0e6e3fcd2c42a76a44481083ffdccb
Iyan S. yue <shiyanpro@gmail.com>
2026-07-07 20:35:37 +0700

    Refactor CONFIG object in config.js
    Removed SITE_TAGLINE from configuration.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
-// Movie Free by R - Configuration
-const CONFIG = {
-    API_BASE: 'https://api.sonzaix.indevs.in',
-    SITE_NAME: 'Movie Free by R',
-    SITE_TAGLINE: 'Nonton Movie & Series Gratis',
-    PER_PAGE: 20,
-    HERO_BANNER_COUNT: 6,
+var CONFIG = {
+  API_BASE: 'https://api.sonzaix.indevs.in',
+  SITE_NAME: 'Movie Free by R',
+  PER_PAGE: 20,
+  HERO_BANNER_COUNT: 6
 };
